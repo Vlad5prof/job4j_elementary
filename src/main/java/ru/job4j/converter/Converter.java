797e1d3889f772e7db4c187befd8d51d02cbbd91
Acer <logiv09@gmail.com>
2024-01-29 00:59:48 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -5,13 +5,12 @@ public class Converter {
         float result = value / 70;
         return result;
     }
-
     public static float rubleToDollar(float value) {
         float result = value / 60;
         return result;
     }
 
-    public static float еuroToDollar(float value) {
+    public static float euroToDollar(float value) {
         float result = value * 2.0f;
         return result;
     }
@@ -20,7 +19,6 @@ public class Converter {
         float result = value * 0.9f;
         return result;
     }
-
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(250);
         System.out.println("250 rubles are " + euro + " euro.");
@@ -28,7 +26,7 @@ public class Converter {
         System.out.println("300 rubles are " + dollars + " dollars.");
         dollars = Converter.dollarToEuro(270);
         System.out.println("270 euro are " + dollars + " dollars.");
-        euro = Converter.еuroToDollar(300);
+        euro = Converter.euroToDollar(300);
         System.out.println("300 dollars are " + euro + " euro ");
     }
 }
