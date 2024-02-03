a4fee4fbb7bf5dc8a3ef2c652cae3aab03a7278c
Acer <logiv09@gmail.com>
2024-02-03 17:30:49 +0300

    5. Конвертер валюты [#41583 #477342]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,33 +1,40 @@
 package ru.job4j.converter;
 
 public class Converter {
+
     public static float rubleToEuro(float value) {
-        float result = value / 70;
-        return result;
-    }
-    public static float rubleToDollar(float value) {
-        float result = value / 60;
-        return result;
+
+        float resultE = value / 70; /* формула перевода рублей в евро. */
+        return resultE;
     }
 
-    public static float euroToDollar(float value) {
-        float result = value * 2.0f;
-        return result;
+    public static float rubleToDollar(float value) {
+
+        float resultD = value / 60; /* формула перевода рублей в доллары. */
+        return resultD;
+    }
+
+    public static float еuroToDollar(float value) {
+
+        float resultED = value * 2.0f; /* формула перевода евро в доллары. */
+        return resultED;
     }
 
     public static float dollarToEuro(float value) {
-        float result = value * 0.9f;
-        return result;
+
+        float resultDE = value * 0.9f; /* формула перевода долларов в евро. */
+        return resultDE;
     }
+
     public static void main(String[] args) {
+
         float euro = Converter.rubleToEuro(250);
-        System.out.println("250 rubles are " + euro + " euro.");
+        System.out.println("250 rubles are " + euro + " euro");
         float dollars = Converter.rubleToDollar(300);
-        System.out.println("300 rubles are " + dollars + " dollars.");
-        dollars = Converter.dollarToEuro(270);
+        System.out.println("300 rubles are " + dollars + " dollars");
+        dollars = Converter.dollarToEuro (270);
         System.out.println("270 euro are " + dollars + " dollars.");
-        euro = Converter.euroToDollar(300);
-        System.out.println("300 dollars are " + euro + " euro ");
+        euro = Converter.dollarToEuro (300);
+        System.out.println("300 dollars are " + euro + " euro");
     }
 }
-
