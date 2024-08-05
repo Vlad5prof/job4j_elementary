f1888022a04b561f69dc1c55a3730cd128882eb4
Acer <logiv09@gmail.com>
2024-08-05 22:59:23 +0300

    4.4. Вложенный оператор if else. Запутанный код [#190]

---
+++ src/test/java/ru/job4j/condition/DivideBySixTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DivideBySixTest {
+
+    @Test
+    void checkNumber() {
+    }
+}
