36a8ad65dbb0db1ed052ea57124924f7b4f21673
Acer <logiv09@gmail.com>
2024-08-05 20:57:17 +0300

    4.3.6. Math.abs [#373616]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -4,16 +4,13 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int result = 0;
         if (isValid(x1) && isValid(x2) && isValid(y1) && isValid(y2)) {
-            if ((x1 <= x2 && y1 <= y2) || (x1 >= x2 && y1 >= y2)
-                    || (x1 > x2 && y1 < y2) || (x1 < x2 && y1 > y2)) {
-                if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
-                    result = Math.abs(x2 - x1);
-                } else {
-                    result = 0;
-                }
+            result = Math.abs(x2 - x1);
+            result = result == Math.abs(y2 - y1) ? result : 0;
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+                result = Math.abs(x2 - x1);
             }
         }
-    return result;
+        return result;
     }
 
     private static boolean isValid(int coordinate) {
