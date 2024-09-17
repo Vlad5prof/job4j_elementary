072bea745a64097410dd5b3aeae8691e1228982b
Acer <logiv09@gmail.com>
2024-09-17 14:52:05 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,10 +9,24 @@ public class Counter {
         return sum;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+             if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        System.out.println(sum(0, 25));
+        System.out.println(sum(0, 20));
         System.out.println(sum(3, 7));
         System.out.println(sum(3, 3));
-        System.out.println(sum(9, 2));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(9, 59));
+        System.out.println(sumByEven(7, 7));
+        System.out.println(sumByEven(-2, 32));
     }
 }
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -6,38 +6,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CounterTest {
 
     @Test
-    void when0Then25() {
+    void whenSumEvenNumbersFromZeroToTenThenThirty() {
         int start = 0;
-        int finish = 25;
-        int result = Counter.sum(start, finish);
-        int expected = 325;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void when3Then7() {
-        int start = 3;
+    void whenSumEvenNumbersFromNineTo59Then850() {
+        int start = 9;
+        int finish = 59;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 850;
+        assertThat(result).isEqualTo(expected);
+        }
+
+    @Test
+    void whenSumEvenNumbersFromSevenToSevenThenZero() {
+        int start = 7;
         int finish = 7;
-        int result = Counter.sum(start, finish);
-        int expected = 25;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 0;
         assertThat(result).isEqualTo(expected);
         }
 
     @Test
-    void whenSumEven() {
-        int start = 3;
-        int finish = 3;
-        int result = Counter.sum(start, finish);
-        int expected = 3;
-        assertThat(result).isEqualTo(expected);
-        }
-
-    @Test
-    void when9Then2() {
-          int start = 9;
-          int finish = 2;
-          int result = Counter.sum(start, finish);
-          int expected = 0;
+    void whenSumEvenNumbersFromMinus2To32Then270() {
+          int start = -2;
+          int finish = 32;
+          int result = Counter.sumByEven(start, finish);
+          int expected = 270;
           assertThat(result).isEqualTo(expected);
           }
     }
